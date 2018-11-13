c7015253fff72554b50702cdf2b2440a8583179a
basimar <basil.marti@unibas.ch>
2018-11-13 13:22:41 +0100

    Script für Abfüllen der Hierarchien in Zip-Dateien

---
+++ package.sh
@@ -0,0 +1,14 @@
+cd output/validation
+zip Basel-UB.zip *Basel-UB_*
+zip Basel-SWA.zip *Basel-UB-Wirtschaft*
+zip Bern-Gosteli.zip *Bern-Gosteli*
+zip Bern-UB-Medizing.zip *Bern-UB-Me*
+zip Bern-UB-SOB.zip *Bern-UB-Schwe*
+zip Aargau-KB.zip *KB-Aargau*
+zip Thurgau-KB.zip *KB-Thurgau*
+zip Appenzell-KB.zip *KB-Appen*
+zip Luzern-ZHB.zip *Luzern-ZHB*
+zip Solothurn-ZB.zip *Solothurn-ZB*
+zip StGallen-SB.zip *St-Gallen-Stif*
+zip Zofingen-SB.zip *Zofingen-SB*
+
